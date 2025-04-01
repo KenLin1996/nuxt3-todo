dda994a37ee9a65b1f7777d443a62c8e9833da5c
Ken Lin <jesus0910727994@gmail.com>
2025-04-01 17:08:27 +0800

    Initialize Nuxt 3 project

---
+++ pages/index.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="container mx-auto p-4">
+    <h1 class="text-2xl font-bold text-center mb-4">Nuxt 3 Todo List</h1>
+    <div class="bg-white p-4 shadow rounded-lg">
+      <p class="text-gray-500">🚀 Todo 列表將顯示在這裡</p>
+    </div>
+  </div>
+</template>
